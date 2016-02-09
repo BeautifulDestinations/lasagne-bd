a1508a4157829eb059735bf915e9b4fbd32f75d2
PGrothaus <philippgrothaus@web.de>
2016-02-09 11:50:17 +0000

    added Spatial Pyramid Pooling Layer in layers/special.py

--- lasagne/layers/special.py
+++ lasagne/layers/special.py
@@ -1066,3 +1066,41 @@ def rrelu(layer, **kwargs):
     if nonlinearity is not None:
         layer.nonlinearity = nonlinearities.identity
     return RandomizedRectifierLayer(layer, **kwargs)
+
+
+class SPPLayer(Layer):
+    '''
+    Spatial Pyramid Pooling Layer.
+    It pools the convolution output of an image of arbitrary size into an array of fixed length.
+    This allows the dense part of the network to be fixed whereas
+    the input dimensions of the image may vary.
+
+    This implementation is a 3-level SPP.
+    Different implementations are possible
+    '''
+    def __init__(self, incoming, **kwargs):
+        super(SPPLayer, self).__init__(incoming, **kwargs)
+
+        # 3x3
+        self.win1 = (int(np.floor(incoming.output_shape[2]/3.0)), int(np.floor(incoming.output_shape[3]/3.0)))
+        self.str1 = (int(np.ceil(incoming.output_shape[2]/3.0)), int(np.ceil(incoming.output_shape[3]/3.0)))
+
+        # 2x2
+        self.win2 = (int(np.floor(incoming.output_shape[2]/2.0)), int(np.floor(incoming.output_shape[3]/2.0)))
+        self.str2 = (int(np.ceil(incoming.output_shape[2]/2.0)), int(np.ceil(incoming.output_shape[3]/2.0)))
+
+        # no divide is one max patch, this is achieved by just doing T.maximum after reshaping
+
+    def get_output_for(self, input, **kwargs):
+        # 3x3
+        p1 = T.reshape(downsample.max_pool_2d(input, ds=self.win1, st=self.str1), (input.shape[0], input.shape[1], 16))
+        # 2x2
+        p2 = T.reshape(downsample.max_pool_2d(input, ds=self.win2, st=self.str2), (input.shape[0], input.shape[1], 4))
+        # 1x1
+        r3 = T.reshape(input, (input.shape[0], input.shape[1], input.shape[2]*input.shape[3]))
+        p3 = T.reshape(T.max(r3, axis=2), (input.shape[0], input.shape[1], 1))
+        return T.concatenate((p1, p2, p3), axis=2)
+
+    def get_output_shape_for(self, input_shape):
+        # (batch_size, num_filters, 3*3+2*2+1*1=14 )
+        return (input_shape[0], input_shape[1], 14 )
